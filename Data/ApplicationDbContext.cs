f000dfd9ad3497767f427dc6c1bf7023fba6af8d
idekferewf <intellligency@mail.ru>
2025-03-09 16:06:33 +0300

    feat: подключена база данных

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using RecipeCatalog.Model;
+
+namespace RecipeCatalog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Recipe> Recipes { get; set; } // таблица Recipe, содержащая данные о рецептах.
+        public DbSet<Ingredient> Ingredients { get; set; } // таблица Ingredient, содержащая данные об ингредиентах.
+        public DbSet<Category> Categories { get; set; } // таблица Category, содержащая данные о категориях рецептов.
+        public DbSet<RecipeIngredient> RecipeIngredients { get; set; } // таблица RecipeIngredient, связующая таблицы Recipe и Ingredient.
+    }
+}
