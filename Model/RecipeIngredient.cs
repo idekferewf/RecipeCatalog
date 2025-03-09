1d9bc147670d836d20fedaf647868c29dc02ba35
idekferewf <intellligency@mail.ru>
2025-03-09 15:41:55 +0300

    feat: добавлена модель, связующая ингредиенты с рецептами

---
+++ Model/RecipeIngredient.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeCatalog.Model
+{
+    public class RecipeIngredient
+    {
+        [Required]
+        public required int Id { get; set; }
+
+        [Required]
+        public required int RecipeId { get; set; }
+
+        [Required]
+        [ForeignKey("RecipeId")]
+        public required Recipe Recipe { get; set; }
+
+        [Required]
+        public required int IngredientId { get; set; }
+
+        [Required]
+        [ForeignKey("IngredientId")]
+        public required Ingredient Ingredient { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(6, 3)")]
+        public required decimal Quantity { get; set; }
+    }
+}
