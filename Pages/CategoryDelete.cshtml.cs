312fa0f2c6a218bba9258a672b71f465d893d0fd
idekferewf <skadeshopp@gmail.com>
2025-03-14 09:13:00 +0300

    feat: добавлена страница с удалением категории

---
+++ Pages/CategoryDelete.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RecipeCatalog.Data;
+using RecipeCatalog.Model;
+
+namespace RecipeCatalog.Pages
+{
+    public class CategoryDeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryDeleteModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Category? category = _context.Categories.FirstOrDefault(b => b.Id == id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+            }
+            return RedirectToPage("Categories");
+        }
+    }
+}
