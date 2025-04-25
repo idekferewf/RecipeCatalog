f8d8c82c43028d736c685f80beca3bae07794d12
dmtrdv <st53@gmail.com>
2025-04-25 08:30:08 +0300

    feat: добавлены модели для авторизации и регистрации (Login и Register)

---
+++ Model/Auth/Register.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeCatalog.Model.Auth;
+
+public class Register
+{
+    [Required(ErrorMessage = "Необходимо указать электронную почту.")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "Необходимо указать пароль.")]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("Password", ErrorMessage = "Пароли не совпадают.")]
+    public string ConfirmPassword { get; set; }
+}
